59626e5ffa0cfc4b7436489b7344ad6fa76e604f
Volatiana Estelle <estelleherimanana4@gmail.com>
2026-07-20 08:13:50 +0300

    init

---
+++ mobilemoney/app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'nom' => 'Jean',
+                'email' => 'jean@gmail.com',
+                'telephone' => '0340000000',
+                'mdp' => password_hash('1234', PASSWORD_DEFAULT)
+            ],
+            [
+                'nom' => 'Marie',
+                'email' => 'marie@gmail.com',
+                'telephone' => '0320000000',
+                'mdp' => password_hash('1234', PASSWORD_DEFAULT)
+            ]
+        ];
+
+        $this->db->table('clients')->insertBatch($data);
+    }
+}
---
+++ mobilemoney/database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE clients(
+    id INT PRIMARY KEY,
+    nom VARCHAR(25),
+    email VARCHAR(50),
+    telephone VARCHAR(15),
+    mdp VARCHAR(25)
+);
+
