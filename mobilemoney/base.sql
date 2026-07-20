bafb2aeb8b8cd3e1f77712d1118bdfb8855291f0
Andry Fanantenana <andryrandria528@gmail.com>
2026-07-20 08:55:01 +0300

    V1_base

--- mobilemoney/base.sql
+++ mobilemoney/base.sql
@@ -1,31 +1,31 @@
 CREATE TABLE prefixe(
     id INT PRIMARY KEY,
-    code VARCHAR(3) NOT NULL
+    code VARCHAR(3) NOT NULL UNIQUE
 );
+
 
 CREATE TABLE client(
     id INT PRIMARY KEY,
-    numero VARCHAR(20) NOT NULL,
-    id_prefixe INT NOT NULL,
-    solde DECIMAL(10, 2) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_prefixe) REFERENCES prefixe(id)
+    numero VARCHAR(20) NOT NULL UNIQUE,
+    solde DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE type_operation(
     id INT PRIMARY KEY,
-    libelle VARCHAR(50) NOT NULL
+    libelle VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE barem_frais(
+CREATE TABLE bareme_frais(
     id INT PRIMARY KEY,
     id_type_operation INT NOT NULL,
     montant_min DECIMAL(10, 2) NOT NULL,
     montant_max DECIMAL(10, 2) NOT NULL,
-    frais DECIMAL(10, 2) NOT NULL
+    frais DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (id_type_operation) REFERENCES type_operation(id)
 );
 
-CREATE TABLE transaction(
+CREATE TABLE operation(
     id INT PRIMARY KEY,
     id_client_source INT NOT NULL,
     id_client_destinataire INT,
